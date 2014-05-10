dff10e2b922e46e0a8d28a4dc8dfaac7e85a992f
wxcl <xcl@cpnv.ch>
2014-05-10 12:39:39 +0200

    myclass

---
+++ src/cpnv/jav1/lima/myclass.java
@@ -0,0 +1,22 @@
+package cpnv.jav1.lima;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import android.util.Log;
+import android.widget.Toast;
+
+/** Class that allows access to the Lima database through a web service
+*/
+public class Myclass {
+	
+	/** brand new class fromwxcl
+	*/
+	
+}
